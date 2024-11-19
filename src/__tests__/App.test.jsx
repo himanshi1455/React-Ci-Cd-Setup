14af8d5cf46ede7ba9a995c0e81f1adc184d8bc5
himanshi <himanshikumawat48@gmail.com>
2024-11-19 15:35:54 +0530

    added codes

---
+++ src/__tests__/App.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import App from '../App';
+
+
+test('renders a component', () =>{
+    render(<App />);
+    // const linkElement = screen.getByText(/Vite/i);
+    // expect(linkElement).toBeInTheDocument();
+});
